90947e9d2f820de12634c00c4d77779b07aa6265
AUTO <auto@dummy.ca>
2015-02-14 16:08:44 -0500

    Build automatique - 20150214-16:08:43

--- frequencier/src/frequencier.cpp
+++ frequencier/src/frequencier.cpp
@@ -77,6 +77,7 @@ void traiterLigneCommande(int argc, char** argv) {
                 if(isalnum(caractereCourant)){
                     motLu.push_back(caractereCourant);
                     
+                    
                 }
                 else{
                    
