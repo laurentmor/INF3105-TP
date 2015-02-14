100b2c4be67c3f1cf568d2a5062d38d497a3d506
AUTO <auto@dummy.ca>
2015-02-14 12:37:54 -0500

    Build automatique - 20150214-12:37:54

--- frequencier/src/frequencier.cpp
+++ frequencier/src/frequencier.cpp
@@ -66,7 +66,7 @@ void traiterLigneCommande(int argc, char** argv) {
             //dans la formation d'un mot
             char caractereCourant;
             string motLu;
-            locale loc;
+            
             while (fichier >> caractereCourant) {
                 if(isalnum(caractereCourant))cout<<"true"<<endl;
                 else cout<<"False"<<endl;
