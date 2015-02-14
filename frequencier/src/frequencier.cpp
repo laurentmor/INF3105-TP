f93e6954abb4664b7f245f831ed31ab57bc6652b
AUTO <auto@dummy.ca>
2015-02-14 12:30:27 -0500

    Build automatique - 20150214-17:30:27

--- frequencier/src/frequencier.cpp
+++ frequencier/src/frequencier.cpp
@@ -68,7 +68,8 @@ void traiterLigneCommande(int argc, char** argv) {
             string motLu;
             locale loc;
             while (fichier >> caractereCourant) {
-                cout << caractereCourant << '\n';
+                cout << isalnum(caractereCourant) << '\n';
+                
                     
                 
             }
