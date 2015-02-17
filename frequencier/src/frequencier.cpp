3e198a885e42f7c7943efbec9d0d7515e1f98052
AUTO <auto@dummy.ca>
2015-02-17 12:32:27 -0500

    Build automatique - 20150217-17:32:27

--- frequencier/src/frequencier.cpp
+++ frequencier/src/frequencier.cpp
@@ -280,7 +280,7 @@ void afficherCouples(vector<Couple>& couples) {
     for (vector<Couple>::iterator it = couples.begin();
             it != couples.end(); ++it) {
         cout << setw(10) << right << it->frequence << " "
-                << left << it->mot << endl;
+                << left << it->mot << '\r\n';
     }
 }
 
